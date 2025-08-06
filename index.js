d694a5cda2ae55e519516e7e0c2b21cafaa2a96c
FarhanBeacon <farhanahmedbeacon@gmail.com>
2025-08-06 19:27:13 +0600

    added some auth and job related routes

---
+++ index.js
@@ -0,0 +1,136 @@
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vasbeiy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+    const jobsCollections = client.db("jobPortal").collection("jobs");
+    const jobApplicationsCollections = client.db("jobPortal").collection("jobApplications");
+
+    // Auth Related APIs
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: '1h'});
+      // To generate a token, in terminal run:
+      // 'node' and then run: 'require('crypto').randomBytes(64).toString('hex')'
+      res.cookie("token", token, {
+        httpOnly: true,
+        secure: false,
+        // sameSite: "Strict",
+        // maxAge: 3600000, // 1 hour
+      })
+      .send({ success: true });
+    })
+
+    // Job Related APIs
+    app.get("/jobs", async (req, res) => {
+      const email = req.query.email;
+      let query = {};
+      if (email) {
+        query = { hr_email: email };
+      }
+      const cursor = jobsCollections.find(query);
+      const jobs = await cursor.toArray();
+      res.send(jobs);
+    });
+
+    app.get("/jobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const job = await jobsCollections.findOne(query);
+      res.send(job);
+    });
+
+    app.post("/jobs", async (req, res)=>{
+      const job = req.body;
+      const result = await jobsCollections.insertOne(job);
+      res.send(result);
+    })
+
+    app.post("/jobApplications", async (req, res) => {
+      const application = req.body;
+      const result = await jobApplicationsCollections.insertOne(application);
+      res.send(result);
+    });
+
+    app.get("/jobApplications/job/:JobId", async (req, res)=>{
+      const jobId = req.params.JobId;
+      query = { JobId: jobId };
+      const cursor = jobApplicationsCollections.find(query);
+      const applications = await cursor.toArray();
+      res.send(applications);
+    })
+
+    app.patch("/jobApplications/:id", async (req, res)=>{
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          Status: req.body.status,
+        }
+      }
+      const result = await jobApplicationsCollections.updateOne(filter, updateDoc);
+      res.send(result);
+    })
+
+    // Get job applications by email
+// Example: http://localhost:5000/jobApplications?email=farhanahmedbeacon@gmail.com
+    app.get("/jobApplications", async (req, res) => {
+      const email = req.query.email;
+      const query = { Applicant_Email: email };
+      const cursor = jobApplicationsCollections.find(query);
+      const applications = await cursor.toArray();
+
+      // It's not a professional way to fetch job details in a loop.
+      for (const application of applications) {
+        const jobQuery = { _id: new ObjectId(application.JobId) };
+        const job = await jobsCollections.findOne(jobQuery);
+        if (job) {
+          application.JobTitle = job.title;
+          application.CompanyName = job.company;
+          application.JobLocation = job.location;
+          application.CompanyLogo = job.company_logo;
+        }
+      }
+      res.send(applications);
+    });
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Server Running");
+});
+
+app.listen(port);
